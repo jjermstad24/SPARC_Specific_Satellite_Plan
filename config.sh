59c4f95d0d3cbd5f3a7d400288f7cfd2aec358a1
root <root@DESKTOP-GQCV9CR.localdomain>
2024-08-22 13:36:05 -0500

    Made module installation specific to allow package to work

--- config.sh
+++ config.sh
@@ -4,10 +4,13 @@ cd ..
 cd orbitpy
 make
 cd ..
-# pip install tqdm
-# pip install pyscipopt
-# pip install opencv-python 
-# pip install Basemap
-# pip install imageio
-# pip install h5py
-# pip install cartopy
+pip install numpy
+pip install pandas==2.0.3
+pip install tqdm
+pip install pyscipopt
+pip install opencv-python 
+pip install Basemap
+pip install imageio
+pip install h5py
+pip install astropy==5.2.2
+pip install cartopy
